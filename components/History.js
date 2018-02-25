079363e087c446902838f03812d5eb798c3674c5
Unknown <chausme@gmail.com>
2018-02-25 11:18:00 +0400

    Complete Lesson 4, add Live.js

--- components/History.js
+++ components/History.js
@@ -38,10 +38,13 @@ class History extends Component {
             </Text>
           </View>
         : <TouchableOpacity
-            onPress={() => console.log('Pressed!')}
+            onPress={() => this.props.navigation.navigate(
+              'EntryDetail',
+              { entryId: key }
+            )}
           >
             <MetricCard date={formattedDate} metrics={metrics} />
-          </TouchableOpacity>}
+        </TouchableOpacity>}
     </View>
   )
   renderEmptyDate(formattedDate) {
